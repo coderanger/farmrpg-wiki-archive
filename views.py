61f5ab4afc44ec6921510f194dd1e705e16f73f8
Noah Kantrowitz <noah@coderanger.net>
2022-03-29 19:24:12 -0700

    ✨ A few new flavors of view tracking CSVs.

--- views.py
+++ views.py
@@ -1,39 +1,94 @@
 import csv
 import datetime
+import itertools
 import json
 from pathlib import Path
 
-from git import Repo
+from git.repo import Repo
 
 ROOT_PATH = (Path(__file__) / "..").resolve()
+
+ViewsData = list[tuple[dict[str, datetime.datetime], dict[str, int]]]
+
+
+def write_csv(
+    data: ViewsData,
+    path: Path,
+    all_pages: set[str],
+):
+    with path.open("w") as outf:
+        writer = csv.DictWriter(outf, list(data[0][0].keys()) + sorted(all_pages))
+        writer.writeheader()
+        for timestamps, views in data:
+            writer.writerow(
+                {
+                    **{label: ts.isoformat() for label, ts in timestamps.items()},
+                    **views,
+                }
+            )
+
+
+def write_flat_csv(
+    data: ViewsData,
+    path: Path,
+):
+    with path.open("w") as outf:
+        writer = csv.DictWriter(outf, list(data[0][0].keys()) + ["Page", "Views"])
+        writer.writeheader()
+        for timestamps, views in data:
+            for page, page_views in views.items():
+                writer.writerow(
+                    {
+                        **{label: ts.isoformat() for label, ts in timestamps.items()},
+                        "Page": page,
+                        "Views": page_views,
+                    }
+                )
 
 
 def pull_views():
     # Parse the Git history for views over time.
     all_pages: set[str] = set()
-    data: list[tuple[datetime.datetime, dict[str, int]]] = []
+    data: ViewsData = []
     repo = Repo(str(ROOT_PATH))
     for commit in repo.iter_commits("main"):
-        if "Latest data:" not in commit.message:
+        message = (
+            commit.message
+            if isinstance(commit.message, str)
+            else commit.message.decode()
+        )
+        if "Latest data:" not in message:
             continue
         try:
-            blob = commit.tree / "views" / "current.json"
+            blob = commit.tree / "views" / "current.json"  # type: ignore
         except KeyError:
             # Too early, ignoring.
             continue
         views: dict[str, int] = json.load(blob.data_stream)
         all_pages |= views.keys()
-        data.append((commit.authored_datetime, views))
+        data.append(({"Timestamp": commit.authored_datetime}, views))
     assert "Timestamp" not in all_pages
+    data.sort(key=lambda d: d[0]["Timestamp"])
     # Generate a CSV file with all view data.
     # Columns are all the pages, rows are timestamps.
-    csv_path = ROOT_PATH / "views" / "views.csv"
-    with csv_path.open("w") as outf:
-        writer = csv.DictWriter(outf, ["Timestamp"] + sorted(all_pages))
-        writer.writeheader()
-        for timestamp, views in data:
-            views["Timestamp"] = timestamp.isoformat()
-            writer.writerow(views)
+    write_csv(data, ROOT_PATH / "views" / "views.csv", all_pages)
+    # Generate a CSV file with the view diffs.
+    diffs: ViewsData = []
+    for ((timestamp1, views1), (timestamp2, views2)) in itertools.pairwise(data):
+        view_diff = {
+            p: views2.get(p, 0) - views1.get(p, 0)
+            for p in views1.keys() | views2.keys()
+        }
+        diffs.append(
+            (
+                {"From": timestamp1["Timestamp"], "To": timestamp2["Timestamp"]},
+                view_diff,
+            )
+        )
+    write_csv(diffs, ROOT_PATH / "views" / "diffs.csv", all_pages)
+    # Variant files for each that are in a simpler layout.
+    write_flat_csv(data, ROOT_PATH / "views" / "flat_views.csv")
+    write_flat_csv(diffs, ROOT_PATH / "views" / "flat_diffs.csv")
 
 
 if __name__ == "__main__":
