5474ccae66ba56c05141580af2153329bd4b8b91
Noah Kantrowitz <noah@coderanger.net>
2022-05-23 17:13:25 -0700

    ✨ Archive tower progression as well.
    This can be used for a future pretty GIF or something.

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ import yaml
 
 ROOT_PATH = (Path(__file__) / "..").resolve()
 
+client = httpx.Client(
+    base_url="https://farmrpg.com/api/", headers={"User-Agent": "wiki-archive/1.0"}
+)
 
-def sync_page(
-    c: httpx.Client, *, name: str, views: int, updated_datetime: str, **kwargs
-):
+
+def sync_page(name: str, views: int, updated_datetime: str, **kwargs):
     page_path = ROOT_PATH / "wiki" / f"{name.replace('/', '-')}.bbcode"
     frontmatter = {
         "name": name,
@@ -33,7 +35,7 @@ def sync_page(
     if page_content is None:
         # Download the page content.
         print(f"Downloading {name}")
-        resp = c.get(f"library/{urllib.parse.quote(name)}")
+        resp = client.get(f"library/{urllib.parse.quote(name)}")
         resp.raise_for_status()
         page = resp.json()
         page_content = page[0]["content"]
@@ -46,26 +48,34 @@ def sync_page(
 
 
 def sync_wiki():
-    with httpx.Client(
-        base_url="https://farmrpg.com/api/", headers={"User-Agent": "wiki-archive/1.0"}
-    ) as c:
-        resp = c.get("library")
-        resp.raise_for_status()
-        errors = []
-        views = {}
-        for page in resp.json():
-            views[page["name"]] = page["views"]
-            try:
-                sync_page(c, **page)
-            except Exception:
-                errors.append(page["name"])
-                traceback.print_exc(file=sys.stdout)
-        with (ROOT_PATH / "views" / "current.json").open("w") as outf:
-            json.dump(views, outf, indent=2, sort_keys=True)
-        if errors:
-            print("Errors fetching:")
-            print("\n".join(errors))
+    resp = client.get("library")
+    resp.raise_for_status()
+    errors = []
+    views = {}
+    for page in resp.json():
+        views[page["name"]] = page["views"]
+        try:
+            sync_page(**page)
+        except Exception:
+            errors.append(page["name"])
+            traceback.print_exc(file=sys.stdout)
+    with (ROOT_PATH / "views" / "current.json").open("w") as outf:
+        json.dump(views, outf, indent=2, sort_keys=True)
+    if errors:
+        print("Errors fetching:")
+        print("\n".join(errors))
+
+
+def sync_tower():
+    resp = client.get("towercounts")
+    resp.raise_for_status()
+    tower = {i + 1: 0 for i in range(100)}
+    for row in resp.json():
+        tower[row["tower"]] = row["count"]
+    with (ROOT_PATH / "tower" / "current.json").open("w") as outf:
+        json.dump(tower, outf, indent=2, sort_keys=True)
 
 
 if __name__ == "__main__":
     sync_wiki()
+    sync_tower()
